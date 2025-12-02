afcd5d83880c8cd51d34624c8e13fb1e0b27c255
Frank Guchelaar <fguchelaar@gmail.com>
2025-12-02 08:40:22 +0100

    add part1 for Day2 project and initial implementation

---
+++ Day2/Program.fs
@@ -0,0 +1,31 @@
+﻿open System.IO
+
+let isEvenLength str =
+    str |> String.length |> (fun len -> len % 2 = 0)
+
+let split str =
+    let mid = String.length str / 2
+    str[.. mid - 1], str[mid..]
+
+let isInvalid x =
+    let asString = x.ToString()
+
+    isEvenLength asString
+    && let firstHalf, secondHalf = split asString in
+       firstHalf = secondHalf
+
+let parse (s: string) : int64 list =
+    s.Split '-' |> Array.map int64 |> (fun arr -> [ arr[0] .. arr[1] ])
+
+let input = "input.txt"
+
+let ranges =
+    (File.ReadAllText input).Split(',')
+    |> Seq.map parse
+    |> Seq.toList
+
+ranges
+|> List.concat
+|> List.filter isInvalid
+|> List.sum
+|> printf "Part 1: %d"
