f67bd5628b3ed0c8999778b1e54b7b9b646d06fd
Frank Guchelaar <fguchelaar@gmail.com>
2025-12-03 16:16:34 +0100

    cleanup

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,20 +1,4 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-open System
-open System.IO
-
-let findLargestJoltage bank =
-    let digits = bank |> Seq.map (fun c -> int c - int '0')
-    let firstMax = digits |> Seq.take (String.length bank - 1) |> Seq.max
-    let indexOfFirstMax = Seq.findIndex (fun x -> x = firstMax) digits
-    let secondMax = Seq.skip (indexOfFirstMax + 1) digits |> Seq.max
-    // concatenate the two max values as strings and convert to int
-    int (string firstMax + string secondMax)
-
-File.ReadAllLines "input.txt"
-|> Seq.map findLargestJoltage
-|> Seq.sum
-|> printfn "Part 1: %d"
-
+﻿open System.IO
 
 let rec find (size: int) (bank: int64 seq) =
     if size = 0 then
@@ -24,34 +8,26 @@ let rec find (size: int) (bank: int64 seq) =
         let head = Seq.take headCount bank
         let max = Seq.max head
         let maxIndex = Seq.findIndex (fun x -> x = max) head
+
         seq {
             yield max
-            yield! find (size - 1) (Seq.skip (maxIndex+1) bank)
+            yield! find (size - 1) (Seq.skip (maxIndex + 1) bank)
         }
 
-
-// let input = "811111111111119"
-//
-// // call find, concat the result
-// input
-// |> Seq.map (fun c -> int64 c - int64 '0')
-// |> find 3
-// |> Seq.map string
-// |> String.concat ""
-// |> int64
-// |> printfn "Part 2: %A"
-
-
-let findLargestJoltage2 bank =
+let findLargestJoltage size bank =
     bank
     |> Seq.map (fun c -> int64 c - int64 '0')
-    |> find 12
+    |> find size
     |> Seq.map string
     |> String.concat ""
     |> int64
 
 File.ReadAllLines "input.txt"
-|> Seq.map findLargestJoltage2
-// |> printfn "%A"
+|> Seq.map (findLargestJoltage 2)
+|> Seq.sum
+|> printfn "Part 1: %d"
+
+File.ReadAllLines "input.txt"
+|> Seq.map (findLargestJoltage 12)
 |> Seq.sum
 |> printfn "Part 2: %d"
