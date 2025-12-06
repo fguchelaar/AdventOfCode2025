9c06ebf7b2020e7b774bca8a99f9c08cfac6200c
Frank Guchelaar <fguchelaar@gmail.com>
2025-12-06 19:46:39 +0100

    implement part2 logic for arithmetic operations and input parsing; update tests for correctness

--- Day6/Logic.fs
+++ Day6/Logic.fs
@@ -1,14 +1,17 @@
 module Day6.Logic
 
+open System
+open System.Linq
+
 let add a b = a + b
 let multiply a b = a * b
 let perform func (sequence: int64 seq) = sequence |> Seq.reduce func
 
-let useFunc operator =
-    match operator with
+let useFunc (operator: string) =
+    match operator.Trim() with
     | "+" -> add
     | "*" -> multiply
-    | _ -> failwith "Unsupported operator"
+    | _ -> failwith $"Unsupported operator: '%s{operator}'"
 
 let eval (sequence: string seq) =
     let length = Seq.length sequence
@@ -17,11 +20,43 @@ let eval (sequence: string seq) =
     perform func tokens
 
 let parseInput (input: string) =
-    input.Split("\n")
-    |> Seq.map (fun line -> line.Split(" ", System.StringSplitOptions.RemoveEmptyEntries) |> Seq.toArray)
+    input.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.map (fun line -> line.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+    |> Seq.toArray
     |> Seq.transpose
 
 let part1 input =
     input |> parseInput |> Seq.map eval |> Seq.sum
 
-let part2 input = -1
+let parse2 (input: string) =
+    input.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.map _.ToCharArray()
+    |> Seq.transpose
+
+// take the operator of the first line. Then apply it to all lines, until a blank line is found. Then repeat.
+let brute (lines: string seq) : int64 =
+    let mutable total = 0L
+    let mutable func = add
+    let mutable currentNumbers = ResizeArray<int64>()
+
+    // add an extra empty line to force final computation
+    let lines = Seq.append lines (seq { yield " " })
+
+    for line in lines do
+        let operator = line.Last()
+
+        if operator = '+' || operator = '*' then
+            func <- useFunc (operator.ToString())
+            currentNumbers.Clear()
+
+        if line.Trim().Length = 0 then
+            let currentTotal = perform func currentNumbers
+            total <- total + currentTotal
+        else
+            let number = Int64.Parse(line.Trim('+', '*', ' '))
+            currentNumbers.Add(number)
+
+    total
+
+let part2 input =
+    input |> parse2 |> Seq.map (fun chars -> String(chars.ToArray())) |> brute
